ab63f683113ed1b0954bd8a5d1f093c5999210e5
rebwill <47540675+rebwill@users.noreply.github.com>
2020-02-02 12:39:01 -0500

    again

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("/api/", (req, res) => {
   });
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
