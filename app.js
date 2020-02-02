fe825271f952c7df8166b16e036a8de0514a9d55
rebwill <47540675+rebwill@users.noreply.github.com>
2020-02-02 12:25:29 -0500

    added require path

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express"); // require express
 const app = express();
 const fs = require("fs");
+const path = require("path");
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 const seafood = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/seafood.json`)
